a2190529225ec482fbd024f87c24e8108ea45239
Thomas Glezen <tcglezen@gmail.com>
2023-05-18 13:31:30 -0700

    Removed sugguestion

--- question_2.java
+++ question_2.java
@@ -4,7 +4,6 @@ import java.util.List;
 /* 
 In our system, we have explainers and interpreters. Explainers take in things such as the underlying model and the dataset and return meaningful data. This data is then visualized in a format that is useful to the user. For example, there is a Shapley explainer which generates the impact that each feature has on the output for a given datapoint. An interpreter for this would be a bar chart since this well visualizes the data. An example of an interpreter that does not work for Shapley feature importance would be a scatter plot. 
 
-As a result each explainer and interpreter will have a set of tags that are used to for pairing explainers and interpreter such that meaningful results are produced. 
 
 Given the following instances: 
 Explainers: 
@@ -33,43 +32,23 @@ class MatchMaker {
     public List<Interpreter> findMatch(Explainer newExplainer) {
         List<Interpreter> matchingInterpreters = new ArrayList<>();
 
-        for (Interpreter interpreter : interpreters) {
-            for (String tag : interpreter.getTags()) {
-                if (newExplainer.getTags().contains(tag)) {
-                    matchingInterpreters.add(interpreter);
-                }
-            }
-        }
-
         return matchingInterpreters;
     }
 }
 
 class Explainer {
     private String name;
-    private List<String> tags;
 
-    public Explainer(String name, List<String> tags) {
-        this.name = name;
-        this.tags = tags;
-    }
-
-    public List<String> getTags() {
-        return tags;
+    public Explainer(String name) {
+        this.name = name; 
     }
 }
 
 class Interpreter {
     private String name;
-    private List<String> tags;
 
-    public Interpreter(String name, List<String> tags) {
+    public Interpreter(String name) {
         this.name = name;
-        this.tags = tags;
-    }
-
-    public List<String> getTags() {
-        return tags;
     }
 }
 
--- question_2.py
+++ question_2.py
@@ -1,7 +1,6 @@
 """
 In our system, we have explainers and interpreters. Explainers take in things such as the underlying model and the dataset and return meaningful data. This data is then visualized in a format that is useful to the user. For example, there is a Shapley explainer which generates the impact that each feature has on the output for a given datapoint. An interpreter for this would be a bar chart since this well visualizes the data. An example of an interpreter that does not work for Shapley feature importance would be a scatter plot. 
 
-As a result each explainer and interpreter will have a set of tags that are used to for pairing explainers and interpreter such that meaningful results are produced. 
 
 Given the following instances: 
 Explainers: 
@@ -24,6 +23,4 @@ class MatchMaker:
         self.interpreters = interpreters 
 
     def find_match(self, new_explainer) -> List['Interpreter']: 
-        # new_explainer has parameter tags which is a list of strings.
-        # each interpreter in self.interpreter has parameter tags which is a list of strings. 
         pass 
